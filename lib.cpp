27c9a6797b4f8029ed5533d1c379131a7930495d
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-12-03 11:07:11 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool numprimo(int a, int i){
+    if(a % i == 0){
+        if(a == i){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    numprimo(a, i + 1);
+}
